dc70dd03b6590fef7b40b16f0b04c89138be3a39
lei <guolei@me.com>
2020-12-03 09:12:05 +0800

    minizip add swiftPM support

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "R2Streamer",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v12), .tvOS(.v9), .watchOS(.v3)
+    ],
+    products: [
+        .library(name: "Minizip", targets: ["Minizip"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Minizip",
+            dependencies: [],
+            exclude: ["module"],
+            linkerSettings: [
+                .linkedLibrary("z")
+            ]),
+    ]
+)
+
+
